35d746b013dd02eebce6bc80ebc2ab815a0b15c8
Nate Catelli <ncatelli@packetfire.org>
2021-09-19 18:51:05 +0000

    helper methods

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,51 @@
 extern crate alloc;
 use alloc::alloc::{GlobalAlloc, Layout};
 
-pub struct SlabAllocator<T> {
+pub struct Chunk<T> {
+    free_list: usize,
     r#type: core::marker::PhantomData<T>,
 }
 
+pub struct SlabAllocator<T> {
+    r#type: core::marker::PhantomData<T>,
+    start: *mut Chunk<T>,
+    chunks: usize,
+}
+
 impl<T> SlabAllocator<T> {
+    /// Represents the maximum number of chunks allowed in the allocator. This
+    /// is equivalent to the number of bits of the pointer type.
+    const CHUNK_MAX: u8 = (usize::BITS as u8 - 1);
+
     pub const fn new() -> Self {
         Self {
             r#type: core::marker::PhantomData,
+            start: core::ptr::null_mut(),
+            chunks: 0,
         }
     }
 
-    pub unsafe fn init(&mut self, _heap_start: usize, _heap_size: usize) {}
+    pub unsafe fn init(&mut self, chunks: u8) {
+        let chunks = chunks & Self::CHUNK_MAX;
+    }
+
+    /// Returns the minimum required size of the given allocator
+    pub const fn required_size(chunks: u8) -> usize {
+        use core::mem;
+        let chunks = (chunks & Self::CHUNK_MAX) as usize;
+        let header_size = mem::size_of::<Self>();
+        let chunks_size = mem::size_of::<Chunk<T>>() * chunks;
+
+        header_size + chunks_size
+    }
+
+    const fn chunk_mask(&self) -> usize {
+        if self.chunks == 0 {
+            0
+        } else {
+            self.chunks - 1
+        }
+    }
 }
 
 unsafe impl<T> GlobalAlloc for SlabAllocator<T> {
@@ -23,4 +56,22 @@ unsafe impl<T> GlobalAlloc for SlabAllocator<T> {
     }
 
     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_align_test_to_atleast_header_size() {
+        let expected_allocator_overhead = (usize::BITS as usize / 8) * 2;
+        let expected_chunk_overhead = (usize::BITS as usize) / 8;
+
+        (0..=32).into_iter().for_each(|chunks| {
+            assert_eq!(
+                expected_allocator_overhead + (expected_chunk_overhead * (chunks as usize)),
+                SlabAllocator::<u8>::required_size(chunks)
+            )
+        });
+    }
 }
